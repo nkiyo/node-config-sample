490983db7feb91dd95e757234941361849a2bd28
nkiyo <nkiyo@hoge.com>
2020-02-13 08:03:19 +0900

    up

--- sample.js
+++ sample.js
@@ -1,4 +1,11 @@
 const config = require('config');
+const nameConfig = require('./config/beatles-member.json')
 
 const dbconfig = config.get('Customer.dbConfig.host');
 console.log(`${dbconfig}`)
+
+console.log(`keys are ${Object.keys(nameConfig)}.`);
+Object.keys(nameConfig).
+  filter(n => 'birth' in nameConfig[n]).
+  forEach(n => console.log(`birth of ${n} is ${nameConfig[n].birth}.`));
+
